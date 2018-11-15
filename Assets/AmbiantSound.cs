f4164e158f099c6d0cc4994da7fe413131993d51
Louis Perdu <l.perdu@rubika-edu.com>
2018-11-15 17:34:33 +0100

    Fixing scene ands sounds after big merge

---
+++ Assets/AmbiantSound.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmbiantSound : MonoBehaviour {
+
+	public string sound;
+	public SFXManager sFXManager;
+
+	void Start () 
+	{
+		sFXManager.PlaySoundAtPosition(sound, transform.position, 1, 1, true);
+
+	}
+
+}
--- Assets/Scripts/Interface.cs
+++ Assets/Scripts/Interface.cs
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-﻿using UnityEngine;
+using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
@@ -15,8 +14,12 @@ public class Interface : MonoBehaviour {
 	public Sprite buildCursor;
 	public Sprite bridgeCursor;
 
-	[Header("=== General ===")][Space(1)]
+	[Space(1)][Header("=== General ===")][Space(1)]
 	public Text txtMode;
+
+	public Slider userBorderSensibility;
+	public Slider userRotationSensibility;
+	public Slider userGrabSensitivity;
 
 	[Space(1)][Header("=== Error Message ===")][Space(1)]
 	public Text txtError;
@@ -64,83 +67,6 @@ public class Interface : MonoBehaviour {
 		case "Bridge":
 			cursorIcon.sprite = bridgeCursor;
 			break;
-	}
-	}
-}
-=======
-﻿using UnityEngine;
-using UnityEngine.UI;
-using System.Collections;
-using System.Collections.Generic;
-
-public class Interface : MonoBehaviour {
-
-	[Header("=== Cursor ===")][Space(1)]
-	public RectTransform cursorTransform;
-	public Image cursorIcon;
-
-	public Sprite defaultCursor;
-	public Sprite destroyCursor;
-	public Sprite buildCursor;
-	public Sprite bridgeCursor;
-
-	[Space(1)][Header("=== General ===")][Space(1)]
-	public Text txtMode;
-
-	[Space(1)][Header("=== General ===")][Space(1)]
-
-	public Slider userBorderSensibility;
-	public Slider userRotationSensibility;
-	public Slider userGrabSensitivity;
-
-	[Space(1)][Header("=== Error Message ===")][Space(1)]
-	public Text txtError;
-	public RectTransform backgroundError;
-
-    [Header("=== References ===")]
-    [Space(1)]
-    public SFXManager sfxManager;
-
-	public void ShowError(string message)
-	{
-        sfxManager.PlaySound("Error");
-		backgroundError.gameObject.SetActive(true);
-		backgroundError.sizeDelta = new Vector2(15f * message.Length, backgroundError.sizeDelta.y);
-		this.gameObject.SetActive(true);
-		txtError.text = message;
-		StartCoroutine(WaitAndPrint(0.1f * message.Length));
-	}
-
-	IEnumerator WaitAndPrint(float delay)
-    {
-		// HELLO
-        // suspend execution for 5 seconds
-        yield return new WaitForSeconds(delay);
-		backgroundError.gameObject.SetActive(false);
-    }
-
-	public void ChangeCursor(string mode)
-	{
-		cursorIcon.enabled = true;
-		switch (mode) 
-		{
-		case "Default":
-			cursorIcon.sprite = null;
-			cursorIcon.enabled = false;
-			break;
-
-		case "Build":
-			cursorIcon.sprite = buildCursor;
-			break;
-
-		case "Delete":
-			cursorIcon.sprite = destroyCursor;
-			break;
-
-		case "Bridge":
-			cursorIcon.sprite = bridgeCursor;
-			break;
 		}
 	}
 }
->>>>>>> 318b70d473e39a9c3dfa8e8678667b4351754c1a
