4dd590ed7135a898ae5711721a7caa17f70b2ecf
rackover <rackover@live.fr>
2019-01-09 16:43:47 +0100

    Fix GameManager accessing unity variable on init

--- Assets/Scripts/System/GameManager.cs
+++ Assets/Scripts/System/GameManager.cs
@@ -29,7 +29,7 @@ public class GameManager : MonoBehaviour
     public CursorDisplay cursorDisplay;
     [Space(1)]
     [Header("SYSTEM SETTINGS")]
-    public bool DEBUG_MODE = Debug.isDebugBuild;
+    public bool DEBUG_MODE = true;
     public bool ENABLE_LOGS = true;
 
     public static GameManager instance;
