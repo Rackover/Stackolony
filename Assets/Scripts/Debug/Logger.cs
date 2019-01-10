3f95fc7a183beba53f56212f55aa1fe0a305baf3
rackover <rackover@live.fr>
2019-01-10 23:23:03 +0100

    Logger now outputs to unity console on FATAL

--- Assets/Scripts/Debug/Logger.cs
+++ Assets/Scripts/Debug/Logger.cs
@@ -35,6 +35,7 @@ public class Logger : MonoBehaviour
     public static void Throw(params string[] msgs) {
         LogMessage(type.ERROR, new string[1] { "================== FATAL ==================" });
         LogMessage(type.ERROR, msgs);
+        UnityEngine.Debug.LogError(msgs.ToString());
         Crash();
     }
 
