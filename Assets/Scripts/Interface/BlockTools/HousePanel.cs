2245df2f7f51b8bdd871d0e5c8284670458b0fd4
Louis Perdu <l.perdu@rubika-edu.com>
2019-01-12 12:30:33 +0100

    Cleaning Folders and item names.

--- Assets/Scripts/Interface/BlockTools/HousePanel.cs
+++ Assets/Scripts/Interface/BlockTools/HousePanel.cs
@@ -25,7 +25,7 @@ public class HousePanel : FlagPanel
 		for(int i = 0; i < h.acceptedPop.Length; i++)
 		{
 			citizenIcons[i].self.SetActive(true);
-			citizenIcons[i].image.sprite = h.acceptedPop[i].visuals;
+//			citizenIcons[i].image.sprite = h.acceptedPop[i].visuals;
 		}
 	}
 }
