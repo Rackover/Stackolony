cb38d302f472abbb1a4542018f0108d7ebe7c6ca
Louis Perdu <l.perdu@rubika-edu.com>
2019-01-12 11:35:22 +0100

    Adding HousePanel

---
+++ Assets/HousePanel.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class CitizenIcon 
+{
+    public GameObject self;
+    public Image image;
+}
+
+public class HousePanel : FlagPanel 
+{
+	public Text slotText;
+	public CitizenIcon[] citizenIcons;
+
+	public void ShowFlag(House h)
+	{
+		slotText.text = "0 / " + h.slotAmount.ToString();
+		
+		for(int i = 0; i < citizenIcons.Length; i++)
+		{
+			citizenIcons[i].self.SetActive(false);
+		}
+
+		for(int i = 0; i < h.acceptedPop.Length; i++)
+		{
+			citizenIcons[i].self.SetActive(true);
+			citizenIcons[i].image.sprite = h.acceptedPop[i].visuals;
+		}
+	}
+}
