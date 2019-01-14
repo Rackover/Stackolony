5bba92de4ad364762dbc05914055484cfcb3cdb2
Rackover <rackover@live.fr>
2019-01-14 22:45:24 +0100

    Cursor fix

--- Assets/Scripts/System/Library.cs
+++ Assets/Scripts/System/Library.cs
@@ -18,7 +18,8 @@ public class Library : MonoBehaviour
 	public Sprite cursorSprite;
 	public Sprite destroyIcon;
 	public Sprite buildIcon;
-	public Sprite bridgeIcon;
+    public Sprite dragIcon;
+    public Sprite bridgeIcon;
 
 	public BlockScheme GetBlockByID(int id)
 	{	
