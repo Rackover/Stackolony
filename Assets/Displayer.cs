c64ee08768eca93a761684a06b743ef1691c71de
Louis Perdu <l.perdu@rubika-edu.com>
2019-01-19 19:50:48 +0100

    Adding DisplayerManager in the project

---
+++ Assets/Displayer.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public enum View{ Front, Plundge, Top }
+
+public class Displayer : MonoBehaviour 
+{
+	public Camera cam;
+
+	[HideInInspector] public bool available;
+	RawImage feed;
+	GameObject model;
+	RenderTexture cTexture;
+
+	public Texture2D GetRender(int width = 256, int height = 256)
+	{
+		cam.enabled = true;
+
+		RenderTexture renderTexture = new RenderTexture(width, height, 16, RenderTextureFormat.ARGB32);
+		cam.targetTexture = renderTexture;
+		RenderTexture.active = cam.targetTexture;
+		cam.Render();
+
+		Texture2D image = new Texture2D(width, height);
+		image.ReadPixels( new Rect(0, 0, width, height), 0, 0);
+		image.Apply();
+
+		Unstage();
+		return image;
+	}
+	
+	public void Stage(GameObject _model, RawImage _feed, int quality)
+	{
+		cam.enabled = true;
+
+		model = Instantiate(_model, transform.position, Quaternion.identity, transform);
+		feed = _feed;
+		cTexture = new RenderTexture(quality, quality, 16, RenderTextureFormat.ARGB32);
+		feed.texture = cTexture;
+		cam.targetTexture = cTexture;
+		available = false;
+	}
+
+	public void Unstage()
+	{
+		cam.targetTexture = null;
+		cam.enabled = false;
+		Destroy(model);
+		Destroy(cTexture);
+		available = true;
+	}
+
+	void Update()
+	{
+		if(model != null)
+			model.transform.Rotate(Vector3.up);
+	}	
+}
---
+++ Assets/DisplayerManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayerManager : MonoBehaviour 
+{
+	public GameObject displayer;
+	List<Displayer> displayers = new List<Displayer>();
+/*
+	[Header("Test")]
+	public GameObject[] objects;
+	public RawImage[] images;
+
+	void Start()
+	{
+		for(int i = 0; i < objects.Length; i++)
+		{
+			SetRotationFeed(objects[i], images[i]);
+		}
+	}
+
+	[ContextMenu("DEBUG")]
+	void DebugStage()
+	{
+		SetRotationFeed(objects[0], images[0]);
+	}
+*/
+	public Displayer SetRotationFeed(GameObject _model, RawImage _feed, int _quality = 128)
+	{
+		Displayer d = GetDisplayer();
+		d.Stage(_model, _feed, _quality);
+		return d;
+	}
+
+	Displayer GetDisplayer()
+	{
+		foreach(Displayer d in displayers)
+		{
+			if(d.available)
+				return d;
+		}
+		Vector3 newPosition = new Vector3(displayers.Count * 2, 0f, 0f);
+		displayers.Add(Instantiate(displayer, newPosition, Quaternion.identity, transform).GetComponent<Displayer>());
+		return displayers[displayers.Count - 1];
+	}
+}
