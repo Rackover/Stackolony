425844606d906ee498bd26463de71a3cbcbd84fb
Louis Perdu <l.perdu@rubika-edu.com>
2019-01-19 16:44:51 +0100

    Finishing TriggerAccident(int accident = -1)

--- Assets/Scripts/System/CityManager.cs
+++ Assets/Scripts/System/CityManager.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class CityManager : MonoBehaviour {
 
     public string cityName = "Valenciennes";
+    public BlockState[] accidentStates = { BlockState.OnFire, BlockState.OnRiot, BlockState.Damaged };
+
 
     //Assign the best house found to a citizen
     public void AutoHouseCitizen(PopulationManager.Citizen citizen)
@@ -82,15 +84,51 @@ public class CityManager : MonoBehaviour {
         attraction -= house.distanceToGround * 0.2f;
         return attraction;
     }
-
+    
     public void TriggerAccident(int accident = -1)
     {
-        if(accident == -1) accident = Random.Range(0, System.Enum.GetValues(typeof(BlockState)).Length);
-        int rand = Random.Range(0, GameManager.instance.systemManager.AllBlocks.Count);
-        while(GameManager.instance.systemManager.AllBlocks[rand].states.Contains((BlockState)accident))
+        if(GameManager.instance.systemManager.AllBlocks.Count == 0) return;
+
+        if(accident == -1) 
         {
-            rand = Random.Range(0, GameManager.instance.systemManager.AllBlocks.Count);
+            accident = (int)accidentStates[Random.Range(0, accidentStates.Length)];
         }
-        GameManager.instance.systemManager.AllBlocks[rand].AddState((BlockState)accident);
+        else if(accident > System.Enum.GetNames(typeof(BlockState)).Length)
+        {
+            return;
+        }
+        
+        if( IsConsideredAccident( (BlockState)accident) )
+        {
+            int rand = Random.Range(0, GameManager.instance.systemManager.AllBlocks.Count);
+            int blockMet = 0;
+            while( GameManager.instance.systemManager.AllBlocks[rand].states.Contains( (BlockState)accident ))
+            {
+                if(blockMet++ > GameManager.instance.systemManager.AllBlocks.Count) 
+                {
+                    Debug.Log("AllBlock are in bad condition already");
+                    return;
+                }
+
+                rand = Random.Range(0, GameManager.instance.systemManager.AllBlocks.Count);
+            }
+            GameManager.instance.systemManager.AllBlocks[rand].AddState( (BlockState)accident );
+        }
+        else
+        {
+            Debug.Log("This state is not considered as a accident");
+        }
+    }
+
+    bool IsConsideredAccident(BlockState state)
+    {
+        foreach(BlockState s in accidentStates)
+        {
+            if(state == s)
+            {
+                return true;
+            }
+        }
+        return false;
     }
 }
