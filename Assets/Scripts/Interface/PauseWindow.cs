0cf2823e0c76ff1bf0ad6e26decde2405a4b7f6c
rackover <rackover@live.fr>
2019-01-19 18:48:13 +0100

    Moved scripts around to improve structure

---
+++ Assets/Scripts/Interface/PauseWindow.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseWindow : MonoBehaviour {
+
+    int oldTimescale;
+
+    public void Start()
+    {
+        oldTimescale = GameManager.instance.temporality.timeScale;
+        GameManager.instance.temporality.SetTimeScale(0);
+    }
+
+    public void Resume()
+    {
+        Destroy(gameObject);
+        GameManager.instance.temporality.SetTimeScale(oldTimescale);
+    }
+
+    public void Options()
+    {
+        FindObjectOfType<Interface>().SpawnOptionsWindow();
+    }
+
+    public void ExitToMenu()
+    {
+        GameManager.instance.ExitToMenu();
+    }
+}
