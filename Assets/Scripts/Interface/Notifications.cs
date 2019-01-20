98ae5940e2839acc75c0c210a61d42425f88f2a1
rackover <rackover@live.fr>
2019-01-20 16:28:29 +0100

    Premisces of animation system

---
+++ Assets/Scripts/Interface/Notifications.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Notifications : MonoBehaviour {
+
+    public class Notification
+    {
+        public Color mainColor;
+        public string[] values;
+        public int locId;
+
+        Notification(int _l, Color _c, params string[] _v)
+        {
+            mainColor = _c;
+            values = _v;
+            locId = _l;
+        }
+    }
+
+	public void Notify(Notification notification)
+    {
+
+    }
+}
