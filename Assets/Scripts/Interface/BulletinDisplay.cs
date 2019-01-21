1c0c5537d26f4f761f215c2039e58fe2768e8279
rackover <rackover@live.fr>
2019-01-21 22:02:33 +0100

    Remade bulletin system

--- Assets/Scripts/Interface/BulletinDisplay.cs
+++ Assets/Scripts/Interface/BulletinDisplay.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BulletinManager : MonoBehaviour {
+public class BulletinDisplay : MonoBehaviour {
 
     public class Bulletin
     {
