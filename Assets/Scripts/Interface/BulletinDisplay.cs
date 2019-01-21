8c5cc4bd7aaf6f1c8bacb89250bac61352c8b0b2
rackover <rackover@live.fr>
2019-01-21 23:53:42 +0100

    Implemented bulletin management with event trigger placeholder

--- Assets/Scripts/Interface/BulletinDisplay.cs
+++ Assets/Scripts/Interface/BulletinDisplay.cs
@@ -1,20 +1,54 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BulletinDisplay : MonoBehaviour {
 
-    public class Bulletin
-    {
-        int ID = 0;
-        int receptionCycle = 0;
-    }
-
+    public bool hasUnread = false;
+    public GameObject notification;
     public GameObject bulletinWindow;
+
+    public Text title;
+    public Text subTitle;
+    public Text text;
+
+    BulletinsManager.Bulletin currentBulletin;
+
+    public void RefreshBulletin()
+    {
+        if (
+            currentBulletin == null ||
+            GameManager.instance.bulletinsManager.GetBulletin().id != currentBulletin.id
+        ) {
+            currentBulletin = GameManager.instance.bulletinsManager.GetBulletin();
+            Localization loc = GameManager.instance.localization;
+
+            title.text = loc.GetLine("bulletin" + currentBulletin.id.ToString(), "bulletinTitle").ToUpper();
+            subTitle.text = loc.GetLine("bulletin" + currentBulletin.id.ToString(), "bulletinSubtitle");
+            text.text = loc.GetLine("bulletin" + currentBulletin.id.ToString(), "bulletinText");
+
+            SetUnread(!bulletinWindow.activeSelf);
+        }
+    }
 
     public void ToggleBulletinDisplay()
     {
+        SetUnread(false);
         bulletinWindow.SetActive(!bulletinWindow.activeSelf);
     }
 
+    public void SetUnread(bool unread)
+    {
+        hasUnread = unread;
+        notification.SetActive(unread);
+    }
+
+
+    private void Update()
+    {
+        if (!GameManager.instance.IsInGame()) return;
+
+        RefreshBulletin();
+    }
 }
