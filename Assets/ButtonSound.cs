5d32fdf9305e6e7b0772b8e173ec6be5d68e180a
Louis Perdu <l.perdu@rubika-edu.com>
2019-01-29 12:24:51 +0100

    Adding ButtonSound component to varius UI elements(mais au pire c'est pas grave ça prends 2 sec à remettre), playing error sound when a error movement occure in CursorDisplay, adding Localization option title

---
+++ Assets/ButtonSound.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonSound : MonoBehaviour 
+{
+	Button button;
+	public string sound;
+
+	void Awake()
+	{
+		button = GetComponent<Button>();
+		if(button == null) Destroy(button);
+	}
+
+	void Start()
+	{
+		button.onClick.AddListener(PlaySound);
+	}
+
+	public void PlaySound()
+	{
+		GameManager.instance.soundManager.Play(sound);
+	}
+}
