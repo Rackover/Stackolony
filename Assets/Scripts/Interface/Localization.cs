f081c78fced5c338b40bb84aac9869af8425b050
rackover <rackover@live.fr>
2019-02-05 11:52:12 +0100

    More accurate localization error logging

--- Assets/Scripts/Interface/Localization.cs
+++ Assets/Scripts/Interface/Localization.cs
@@ -150,7 +150,7 @@ public class Localization : MonoBehaviour {
             return Interpret(line);
         }
         catch(KeyNotFoundException e) {
-            Logger.Error("Could not load line ID " + id);
+            Logger.Error("Could not load line "+ currentCategory+":"+ id);
             return "[LOC:" + currentCategory + "=>" + id+"]";
         }
     }
