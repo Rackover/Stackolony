0822081601db5c4cf262a279ab78c67c98552509
Louis Perdu <l.perdu@rubika-edu.com>
2019-02-12 18:02:48 +0100

    Cleaning Bystander and animations, creating new Manager and behavior for the Roamers visuals

---
+++ Assets/RoamerBehavior.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoamerBehavior : MonoBehaviour
+{
+	[HideInInspector] public RoamerManager manager;
+	[HideInInspector] public Bystander visual;
+	[HideInInspector] public bool available;
+
+	public List<Vector3> path = new List<Vector3>();
+	int cPoint;
+
+	void Update()
+	{
+		if(cPoint < path.Count)
+		{
+
+		}
+	}
+
+	public void NewPath(List<Vector3> newPath)
+	{
+		path = newPath;
+		cPoint = 0;
+		transform.position = path[0];
+	}
+
+	void MoveToNext()
+	{
+		cPoint++;
+
+		if(cPoint < path.Count)
+		{
+			Debug.Log("Next");
+		}
+		else
+		{
+			Debug.Log("Arrived");
+		}
+	}
+
+	public void LoadVisual()
+	{
+		if(visual != null) Destroy(visual.gameObject);
+
+		visual = Instantiate(
+			manager.bystanderPrefabs[Random.Range(0, manager.bystanderPrefabs.Length)],
+			transform.position,
+			Quaternion.identity,
+			transform
+		).GetComponent<Bystander>();
+	}
+}
---
+++ Assets/RoamerManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoamerManager : MonoBehaviour
+{
+	List<RoamerBehavior> roamers = new List<RoamerBehavior>();
+
+	[Header("Prefabs")]
+	public GameObject roamer;
+	public GameObject[] bystanderPrefabs;
+
+	void Start()
+	{
+		NewRoamer();
+	}
+
+	void NewRoamer()
+	{
+		RoamerBehavior roamer = GetRoamer();
+		roamer.manager = this;
+		roamer.LoadVisual();
+		roamer.NewPath(GetRandomPosition(10));
+		roamer.visual.animator.Play("Running");
+	}
+
+	List<Vector3> GetRandomPosition(int lenght)
+	{
+		List<Vector3> randomPath = new List<Vector3>();
+		for( int i = 0; i < lenght; i++ )
+		{
+			randomPath.Add(new Vector3(Random.Range(-5f, 5f), 0f, Random.Range(-5f, 5f)));
+		}
+		return randomPath;
+	}
+
+	RoamerBehavior GetRoamer()
+	{
+		foreach(RoamerBehavior roamer in roamers)
+		{
+			if(roamer.available) return roamer;
+		}
+		roamers.Add(Instantiate(roamer).GetComponent<RoamerBehavior>());
+		return roamers[roamers.Count - 1];
+	}
+}	
