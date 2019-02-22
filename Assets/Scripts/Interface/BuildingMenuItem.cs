6106fb229a939cc9269f7e808fe02d1994a66e9a
Marolles <julesduvette@gmail.com>
2019-02-22 18:23:58 +0100

    Fixed issue when a player could drag multiple blocks from the menus at the same time

--- Assets/Scripts/Interface/BuildingMenuItem.cs
+++ Assets/Scripts/Interface/BuildingMenuItem.cs
@@ -102,8 +102,9 @@ public class BuildingMenuItem : MonoBehaviour, IPointerEnterHandler, IPointerExi
             draggingBuilding = null;
         }
         else if (!isLocked){
-            if (Input.GetButton("Select") && concerned && !GameManager.instance.cursorManagement.isDragging) {
+            if (Input.GetButton("Select") && concerned && !GameManager.instance.cursorManagement.isDragging && isBeingDragged == false) {
                 isBeingDragged = true;
+                GameManager.instance.cursorManagement.isDragging = true;
                 ri.color = new Color(ri.color.r, ri.color.g, ri.color.b, 0f);
             }
             if (Input.GetButtonUp("Select")) {
